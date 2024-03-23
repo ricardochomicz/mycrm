5b2e70bfb76b4e92b7161974ba05fbc6bd3314f0
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-03-23 16:53:28 -0300

    update model operator

--- app/Models/Operator.php
+++ app/Models/Operator.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,4 +13,29 @@ class Operator extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['name'];
+
+    public function scopeFilter($query, array $filters): void
+    {
+        $query->when($filters['search'] ?? null, function ($query, $search) {
+            $query->where('name', 'LIKE', '%' . $search . '%');
+        })->when($filters['trashed'] ?? null, function ($query, $trashed) {
+            if ($trashed === 'only') {
+                $query->onlyTrashed();
+            }
+        });
+    }
+
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => mb_strtoupper($value, 'UTF-8'),
+        );
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Carbon::parse($value)->format('d/m/Y'),
+        );
+    }
 }
