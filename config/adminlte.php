a581a792aa3aa2c2062d2c702eacf6f7bdfce415
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-03-23 15:30:07 -0300

    Update title

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,7 +63,7 @@ return [
     |
     */
 
-    'logo' => 'My<b>Finances</b>',
+    'logo' => 'My<b>CRM</b>',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
