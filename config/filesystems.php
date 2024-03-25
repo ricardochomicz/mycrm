f5670c671fbce48ab3bdfb3facae38cae06c8453
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-03-25 08:54:55 -0300

    update filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,7 +37,7 @@ return [
         ],
 
         'public' => [
-            'driver' => 'production',
+            'driver' => 'local',
             'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
