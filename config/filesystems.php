fa2b58fe3516ee5fd0f6ba68e894d5ac06b03aa5
Ricardo Chomicz <ricardo.chomicz@gmail.com>
2024-03-25 12:51:18 -0300

    update filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,9 +56,7 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-        'links' => [
-            base_path('public_html/storage') => storage_path('app/public'),
-        ],
+
 
     ],
 
